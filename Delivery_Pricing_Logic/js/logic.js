28a30962f2677422849c6767b2d30ba9c0ce6791
Jonathanmon0429 <jonathanmonterroza803@gmail.com>
2026-04-13 23:55:02 -0600

    Delete Delivery_Pricing_Logic directory

--- Delivery_Pricing_Logic/js/logic.js
+++
@@ -1,6 +0,0 @@
-function shippingCost(){
-    let orderCost = Number(document.getElementById("totalOrder").value);
-    let isPremium = document.getElementById("isPremium").value === "true";
-
-    let shipping = (isPremium || orderCost > 20)?document.getElementById("shippingCost").textContent = "The shipping cost is free":document.getElementById("shippingCost").textContent = "The shipping cost is 2 dollars"
-}
